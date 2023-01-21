f9c34cf1846c814118c18fa65d5e0288c031076a
hunderboy <masury2011@gmail.com>
2023-01-22 03:47:26 +0900

    feature : "text 반환과 객체반환의 비교"
    MVC와 템플릿 엔진 vs API

--- src/main/java/co/kr/steadysprinting/hellospring/controller/HelloController.java
+++ src/main/java/co/kr/steadysprinting/hellospring/controller/HelloController.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
-
 
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model) {
@@ -21,10 +21,29 @@ public class HelloController {
         return "hello-template";
     }
 
-    @GetMapping("hello-string")
-    @ResponseBody
+    @GetMapping("hello-string") // Get 메소드 url
+    @ResponseBody // http 의 body
     public String helloString(@RequestParam("name") String name) {
-        return "hello " + name;
+        return "hello " + name;  // 반환값이 Text 인 경우,템플릿의 html 코드가 아닌 있는 그래도 Text 배출
     }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello(); // command + shift + enter
+        hello.setName(name);
+        return hello; // @ResponseBody 를 사용하고, 반환값이 객체인 경우, JSON으로 변환됨 = {"name":"spring!!"}
+    }
+    static class Hello {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
 
 }

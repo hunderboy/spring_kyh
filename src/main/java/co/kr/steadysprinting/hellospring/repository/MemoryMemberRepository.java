464168f2809730b9da37b7aaccd48b0a33dd13f7
hunderboy <masury2011@gmail.com>
2023-01-22 21:24:39 +0900

    feature : "회원관리 비지니스 요구 사항 코드 추가"
    domain : Member,
    repository : MemberRepository interface -> MemoryMemberRepository

---
+++ src/main/java/co/kr/steadysprinting/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package co.kr.steadysprinting.hellospring.repository;
+
+import co.kr.steadysprinting.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // null 값이 반환 될것을 생각하여 Optional.ofNullable 사용
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name)) // 람다 사용
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values()); // store 의 value 인 Member 모두가 반환 된다.
+    }
+}

9129f172b3223d826257673fdb337a9a1508df6d
hunderboy <masury2011@gmail.com>
2023-04-17 22:26:16 +0900

    feat: "회원 웹 기능 완료"

--- src/main/java/co/kr/steadysprinting/hellospring/controller/MemberController.java
+++ src/main/java/co/kr/steadysprinting/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package co.kr.steadysprinting.hellospring.controller;
 
+import co.kr.steadysprinting.hellospring.domain.Member;
 import co.kr.steadysprinting.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -16,6 +22,33 @@ public class MemberController {
     public MemberController(MemberService memberService) { // 생성자 파라머터로 설정(의존성 주입)
         this.memberService = memberService;
     }
+
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/"; // 회원 가입에 성공하면 home 화면으로 보내겠다는 의미
+    }
+
+
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 
 
 }
